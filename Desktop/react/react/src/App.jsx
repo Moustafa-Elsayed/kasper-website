3eaf474fe4c89797d7718a0e6670e08023996503
Moustafa Elsayed <moelsayed949@gmail.com>
2023-10-11 10:45:52 +0200

    complete responsive navbar

--- Desktop/react/react/src/App.jsx
+++ Desktop/react/react/src/App.jsx
@@ -5,9 +5,13 @@ import Contact from "./pages/contact/Contact";
 import Head from "./pages/navbar/Head";
 import Newform from "./pages/form/Newform";
 import Home from "./pages/home/Home";
+import { useState } from "react";
 // import Error from "./pages/error/Error";
 
 function App() {
+  const [open, setOpen] = useState("none");
+  const [close, setClose] = useState("permanent");
+
   // const [data, setData] = useState("");
   // const getData = () => {
   //   const body = {
@@ -26,7 +30,7 @@ function App() {
   // }, []);
   return (
     <>
-      <Head />
+      <Head open={open} setOpen={setOpen} close={close} setClose={setClose}/>
       {/* <Nav /> */}
       <Routes>
         <Route path="/" element={<Home />} />
--- Desktop/react/react/src/pages/navbar/Head.jsx
+++ Desktop/react/react/src/pages/navbar/Head.jsx
@@ -1,24 +1,108 @@
-import { Box, Button, ListItemButton, ListItemText } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import LoginIcon from "@mui/icons-material/Login";
 import { useNavigate } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
-const Head = () => {
+// eslint-disable-next-line react/prop-types
+const Head = ({ open, setOpen, close, setClose }) => {
   const navigate = useNavigate();
 
   return (
-    <AppBar position="fixed" color="primary">
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Typography>
-          <img src=".\img\logo.png" />
-        </Typography>
+    <>
+      <AppBar position="fixed" color="primary">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography>
+            <img src=".\img\logo.png" />
+          </Typography>
 
-        <Box sx={{ display: { xs: "none", md: "block" } }}>
-          <Box sx={{ display: "flex" }}>
-            <ListItemButton>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
+            <Box sx={{ display: "flex" }}>
+              <ListItemButton>
+                <ListItemText
+                  primary="Home"
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText
+                  primary="About"
+                  onClick={() => {
+                    navigate("/about");
+                  }}
+                />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText
+                  primary="Contact"
+                  onClick={() => {
+                    navigate("/contact");
+                  }}
+                />
+              </ListItemButton>
+
+              <Button
+                startIcon={<LoginIcon />}
+                variant="outlined"
+                color="inherit"
+                onClick={() => {
+                  navigate("/newform");
+                }}
+              >
+                Sign-In
+              </Button>
+            </Box>
+          </Box>
+
+          <MenuIcon
+            onClick={() => {
+              setClose("temporary");
+              setOpen("block");
+            }}
+            sx={{ display: { xs: "block", md: "none" } }}
+          />
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        open={true}
+        onClose={() => {
+          setClose("permanent")
+          setOpen("none");
+        }}
+        sx={{
+          position: "relative",
+          width: 250,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 250,
+            boxSizing: "border-box",
+          },
+          display: { xs: open },
+        
+        }}
+        variant={close}
+        anchor="right"
+      >
+        <CloseIcon sx={{ position: "absolute", right: 13, top: 13 }} />
+
+        <Toolbar />
+        <Divider />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <Box>
+            <ListItemButton sx={{ textAlign: "center", mb: 1 }}>
               <ListItemText
                 primary="Home"
                 onClick={() => {
@@ -26,7 +110,7 @@ const Head = () => {
                 }}
               />
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{ textAlign: "center", mb: 1 }}>
               <ListItemText
                 primary="About"
                 onClick={() => {
@@ -34,7 +118,7 @@ const Head = () => {
                 }}
               />
             </ListItemButton>
-            <ListItemButton>
+            <ListItemButton sx={{ textAlign: "center", mb: 1 }}>
               <ListItemText
                 primary="Contact"
                 onClick={() => {
@@ -44,6 +128,8 @@ const Head = () => {
             </ListItemButton>
 
             <Button
+              sx={{ textAlign: "center", mb: 1 }}
+              fullWidth
               startIcon={<LoginIcon />}
               variant="outlined"
               color="inherit"
@@ -55,9 +141,8 @@ const Head = () => {
             </Button>
           </Box>
         </Box>
-        <MenuIcon sx={{ display: { xs: "block", md: "none" } }} />
-      </Toolbar>
-    </AppBar>
+      </Drawer>
+    </>
   );
 };
 
